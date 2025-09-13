c41b9f02bf58f22486b75510c328462089a41fe1
FlyingCow32 <gael.tournier32@gmail.com>
2025-09-13 20:07:36 +0100

    Animations
    + Added animations with JS on the main page

---
+++ script/script.js
@@ -0,0 +1,28 @@
+const index = document.getElementById('index');
+const presepi = document.getElementById('presepi');
+const stats = document.getElementById('stats');
+const textstats = document.getElementById('textstats');
+
+const elementsToShow = [presepi, stats, textstats];
+
+elementsToShow.forEach(element => {
+     element.style.opacity = '0';
+     element.style.transform = 'translateY(50px)';
+     element.style.transition = 'opacity 0.5s ease-in, transform 0.5s ease-in';
+});
+
+function checkIfVisible() {
+     elementsToShow.forEach(element => {
+          const rect = element.getBoundingClientRect(); //This tag gets the viewport and convert it to number
+          const elementTop = rect.top; 
+          const windowHeight = window.innerHeight;
+
+          if (elementTop < windowHeight / 2){ // If the element position in the viewport is inferior than the window Height
+               element.style.opacity = '1'; // Divided by the number, then set the opacity to 1 and return to the the position
+               element.style.transform ='translateY(0)';
+          }
+     });
+};
+
+window.addEventListener('scroll', checkIfVisible);
+checkIfVisible();
